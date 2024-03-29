8ac41080e5995b86a1245162972edce0c6bb8e20
Anton Vlasenko <vlasenko.anton@gmail.com>
2024-03-29 17:57:04 +0100

    Better violation codes.

--- test/php/gutenberg-coding-standards/Gutenberg/Sniffs/Commenting/FunctionCommentSinceTagSniff.php
+++ test/php/gutenberg-coding-standards/Gutenberg/Sniffs/Commenting/FunctionCommentSinceTagSniff.php
@@ -34,6 +34,21 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	 */
 	public $minimumVisibility = 'private';
 
+	protected static $ooTokens = array(
+		T_CLASS     => array(
+			'name' => 'class',
+		),
+		T_INTERFACE => array(
+			'name' => 'interface',
+		),
+		T_TRAIT     => array(
+			'name' => 'trait',
+		),
+		T_ENUM      => array(
+			'name' => 'enum',
+		),
+	);
+
 	/**
 	 * This property is used to store results returned
 	 * by the static::is_experimental_block() method.
@@ -48,11 +63,13 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	 * @return array<int|string>
 	 */
 	public function register() {
-		return array(
-			T_FUNCTION,
-			T_VARIABLE,
-			T_CLASS,
-			T_STRING
+		return array_merge(
+			array(
+				T_FUNCTION,
+				T_VARIABLE,
+				T_STRING,
+			),
+			array_keys( static::$ooTokens )
 		);
 	}
 
@@ -60,7 +77,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	 * Processes the tokens that this sniff is interested in.
 	 *
 	 * @param File $phpcsFile The file being scanned.
-	 * @param int $stackPtr   The position of the current token in the stack passed in $tokens.
+	 * @param int  $stackPtr  The position of the current token in the stack passed in $tokens.
 	 */
 	public function process( File $phpcsFile, $stackPtr ) {
 		if ( static::is_experimental_block( $phpcsFile ) ) {
@@ -76,8 +93,8 @@ class FunctionCommentSinceTagSniff implements Sniff {
 			return;
 		}
 
-		if ( 'T_CLASS' === $token['type'] ) {
-			$this->process_class_token( $phpcsFile, $stackPtr );
+		if ( in_array( $token['code'], array_keys( static::$ooTokens ), true ) ) {
+			$this->process_oo_token( $phpcsFile, $stackPtr );
 			return;
 		}
 
@@ -86,7 +103,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 			return;
 		}
 
-		if ( Scopes::isOOProperty( $phpcsFile, $stackPtr ) ) {
+		if ( 'T_VARIABLE' === $token['type'] && Scopes::isOOProperty( $phpcsFile, $stackPtr ) ) {
 			$this->process_property_token( $phpcsFile, $stackPtr );
 		}
 	}
@@ -96,7 +113,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	 * to check for a missing `@since` tag in its docblock.
 	 *
 	 * @param File $phpcs_file   The file being scanned.
-	 * @param int $stack_pointer The position of the hook token in the stack.
+	 * @param int  $stack_pointer The position of the hook token in the stack.
 	 */
 	protected function process_hook( File $phpcs_file, $stack_pointer ) {
 		$tokens = $phpcs_file->getTokens();
@@ -122,7 +139,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 			$hook_function,
 		);
 
-		$violation_code = 'missingSinceTag';
+		$violation_code = 'MissingHookSinceTag';
 
 		$docblock = static::find_hook_docblock( $phpcs_file, $stack_pointer );
 		if ( false === $docblock ) {
@@ -154,7 +171,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 		$phpcs_file->addError(
 			'Invalid @since version value for the "%s()" hook: "%s". Version value must be greater than or equal to 0.0.1.',
 			$version_token,
-			'InvalidClassSinceTagVersionValue',
+			'InvalidHookSinceTagVersionValue',
 			array(
 				$hook_function,
 				$version_value,
@@ -162,16 +179,18 @@ class FunctionCommentSinceTagSniff implements Sniff {
 		);
 	}
 
-	protected function process_class_token( File $phpcs_file, $stack_pointer ) {
-		$tokens = $phpcs_file->getTokens();
+	protected function process_oo_token( File $phpcs_file, $stack_pointer ) {
+		$tokens     = $phpcs_file->getTokens();
+		$token_type = static::$ooTokens[ $tokens[ $stack_pointer ]['code'] ]['name'];
 
-		$class_name                      = ObjectDeclarations::getName( $phpcs_file, $stack_pointer );
+		$token_name                      = ObjectDeclarations::getName( $phpcs_file, $stack_pointer );
 		$missing_since_tag_error_message = sprintf(
-			'@since tag is missing for the "%s" class.',
-			$class_name,
+			'@since tag is missing for the "%s" %s.',
+			$token_name,
+			$token_type
 		);
 
-		$violation_code = 'missingSinceTag';
+		$violation_code = 'Missing' . ucfirst( $token_type ) . 'SinceTag';
 
 		$docblock = static::find_docblock( $phpcs_file, $stack_pointer );
 		if ( false === $docblock ) {
@@ -197,9 +216,9 @@ class FunctionCommentSinceTagSniff implements Sniff {
 		$phpcs_file->addError(
 			'Invalid @since version value for the "%s()" hook: "%s". Version value must be greater than or equal to 0.0.1.',
 			$version_token,
-			'InvalidClassSinceTagVersionValue',
+			'Invalid' . ucfirst( $token_type ) . 'SinceTagVersionValue',
 			array(
-				$class_name,
+				$token_name,
 				$version_value,
 			)
 		);
@@ -208,8 +227,8 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	/**
 	 * Processes a token representing an object-oriented property to check for a missing @since tag in its docblock.
 	 *
-	 * @param File $phpcs_file   The file being scanned.
-	 * @param int $stack_pointer The position of the object-oriented property token in the stack.
+	 * @param File $phpcs_file    The file being scanned.
+	 * @param int  $stack_pointer The position of the object-oriented property token in the stack.
 	 */
 	protected function process_property_token( File $phpcs_file, $stack_pointer ) {
 		$tokens = $phpcs_file->getTokens();
@@ -223,13 +242,13 @@ class FunctionCommentSinceTagSniff implements Sniff {
 			$property_name
 		);
 
-		$violation_code = 'missingSinceTag';
+		$violation_code = 'MissingPropertySinceTag';
 
 		$scope_modifier = Variables::getMemberProperties( $phpcs_file, $stack_pointer )['scope'];
 		if ( ( $scope_modifier === 'protected'
-		       && $this->minimumVisibility === 'public' )
-		     || ( $scope_modifier === 'private'
-		          && ( $this->minimumVisibility === 'public' || $this->minimumVisibility === 'protected' ) )
+				&& $this->minimumVisibility === 'public' )
+			|| ( $scope_modifier === 'private'
+					&& ( $this->minimumVisibility === 'public' || $this->minimumVisibility === 'protected' ) )
 		) {
 			return;
 		}
@@ -258,7 +277,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 		$phpcs_file->addError(
 			'Invalid @since version value for the "%s::%s" property: "%s". Version value must be greater than or equal to 0.0.1.',
 			$version_token,
-			'InvalidSinceTagVersionValue',
+			'InvalidPropertySinceTagVersionValue',
 			array(
 				$class_name,
 				$property_name,
@@ -270,8 +289,8 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	/**
 	 * Processes a T_FUNCTION token to check for a missing @since tag in its docblock.
 	 *
-	 * @param File $phpcs_file   The file being scanned.
-	 * @param int $stack_pointer The position of the T_FUNCTION token in the stack.
+	 * @param File $phpcs_file    The file being scanned.
+	 * @param int  $stack_pointer The position of the T_FUNCTION token in the stack.
 	 */
 	protected function process_function_token( File $phpcs_file, $stack_pointer ) {
 		$tokens = $phpcs_file->getTokens();
@@ -285,9 +304,9 @@ class FunctionCommentSinceTagSniff implements Sniff {
 		if ( $is_oo_method ) {
 			$scope_modifier = FunctionDeclarations::getProperties( $phpcs_file, $stack_pointer )['scope'];
 			if ( ( $scope_modifier === 'protected'
-			       && $this->minimumVisibility === 'public' )
-			     || ( $scope_modifier === 'private'
-			          && ( $this->minimumVisibility === 'public' || $this->minimumVisibility === 'protected' ) )
+					&& $this->minimumVisibility === 'public' )
+				|| ( $scope_modifier === 'private'
+						&& ( $this->minimumVisibility === 'public' || $this->minimumVisibility === 'protected' ) )
 			) {
 				return;
 			}
@@ -339,15 +358,15 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	 * Since a line containing a hook can include any type of tokens, this method backtracks through the tokens
 	 * to locate the first token on the current line. This token is then used as the starting point for searching the docblock.
 	 *
-	 * @param File $phpcs_file   The file being scanned.
-	 * @param int $stack_pointer The position to start looking for the docblock.
+	 * @param File $phpcs_file    The file being scanned.
+	 * @param int  $stack_pointer The position to start looking for the docblock.
 	 * @return array|false The start and end tokens of the docblock, or false if not found.
 	 */
 	protected static function find_hook_docblock( File $phpcs_file, $stack_pointer ) {
 		$tokens       = $phpcs_file->getTokens();
 		$current_line = $tokens[ $stack_pointer ]['line'];
 
-		for ( $i = $stack_pointer; $i >= 0; $i -- ) {
+		for ( $i = $stack_pointer; $i >= 0; $i-- ) {
 			if ( $tokens[ $i ]['line'] < $current_line ) {
 				// The previous token is on the previous line, so the current token is the first on the line.
 				return static::find_docblock( $phpcs_file, $i + 1 );
@@ -361,8 +380,8 @@ class FunctionCommentSinceTagSniff implements Sniff {
 	 * Finds the docblock preceding a specified position (stack pointer) in a given PHP file.
 	 * The implementation was copied from FunctionCommentSniff::process().
 	 *
-	 * @param File $phpcs_file   The file being scanned.
-	 * @param int $stack_pointer The position (stack pointer) in the token stack from which to start searching backwards.
+	 * @param File $phpcs_file    The file being scanned.
+	 * @param int  $stack_pointer The position (stack pointer) in the token stack from which to start searching backwards.
 	 * @return array|false An array with the starting and ending token positions of the found docblock, or false if no docblock is found.
 	 */
 	protected static function find_docblock( File $phpcs_file, $stack_pointer ) {
@@ -370,13 +389,13 @@ class FunctionCommentSinceTagSniff implements Sniff {
 		$ignore                 = Tokens::$methodPrefixes;
 		$ignore[ T_WHITESPACE ] = T_WHITESPACE;
 
-		for ( $comment_end = ( $stack_pointer - 1 ); $comment_end >= 0; $comment_end -- ) {
+		for ( $comment_end = ( $stack_pointer - 1 ); $comment_end >= 0; $comment_end-- ) {
 			if ( isset( $ignore[ $tokens[ $comment_end ]['code'] ] ) ) {
 				continue;
 			}
 
 			if ( T_ATTRIBUTE_END === $tokens[ $comment_end ]['code']
-			     && isset( $tokens[ $comment_end ]['attribute_opener'] )
+				&& isset( $tokens[ $comment_end ]['attribute_opener'] )
 			) {
 				$comment_end = $tokens[ $comment_end ]['attribute_opener'];
 				continue;
@@ -398,7 +417,7 @@ class FunctionCommentSinceTagSniff implements Sniff {
 
 		$docblock_end_tags = array(
 			T_DOC_COMMENT_CLOSE_TAG,
-			T_COMMENT
+			T_COMMENT,
 		);
 
 		if ( ! in_array( $tokens[ $comment_end ]['code'], $docblock_end_tags, true ) ) {
@@ -407,16 +426,16 @@ class FunctionCommentSinceTagSniff implements Sniff {
 
 		return array(
 			$tokens[ $comment_end ]['comment_opener'],
-			$comment_end
+			$comment_end,
 		);
 	}
 
 	/**
 	 * Searches for a version tag within a docblock.
 	 *
-	 * @param File $phpcs_file        The file being scanned.
-	 * @param int $docBlockStartToken The token index where the docblock starts.
-	 * @param int $docBlockEndToken   The token index where the docblock ends.
+	 * @param File $phpcs_file         The file being scanned.
+	 * @param int  $docBlockStartToken The token index where the docblock starts.
+	 * @param int  $docBlockEndToken   The token index where the docblock ends.
 	 * @return false|int The token index of the version tag within the docblock if found, false otherwise.
 	 */
 	protected static function find_version_tag_token( File $phpcs_file, $doc_block_start_token, $doc_block_end_token ) {
